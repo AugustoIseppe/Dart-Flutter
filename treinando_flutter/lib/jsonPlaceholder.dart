650a1b741a01f0fb454cabf2f34e83961516e77c
AugustoIseppe <augusto.iseppe@outlook.com>
2023-08-23 23:54:37 -0300

    consumo de dados - api JsonPlaceholder

---
+++ treinando_flutter/lib/jsonPlaceholder.dart
@@ -0,0 +1,80 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+import 'dart:convert' as convert;
+import 'dart:async';
+import 'package:http/http.dart' as http;
+
+class HomeJsonPlaceholder extends StatefulWidget {
+  const HomeJsonPlaceholder({super.key});
+
+  @override
+  State<HomeJsonPlaceholder> createState() => _HomeJsonPlaceholderState();
+}
+
+class _HomeJsonPlaceholderState extends State<HomeJsonPlaceholder> {
+  Future<Map> getData() async {
+    String url = "https://jsonplaceholder.typicode.com/photos/2";
+    http.Response response = await http.get(Uri.parse(url));
+    return convert.jsonDecode(response.body);
+  }
+
+  late String title;
+  late String img;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          title: const Text(
+            'JsonPlaceholder',
+            style: TextStyle(color: Colors.black),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.white,
+          actions: [
+            IconButton(onPressed: () {}, icon: const Icon(Icons.refresh)),
+          ],
+        ),
+        body: FutureBuilder(
+            future: getData(),
+            builder: (context, snapshot) {
+              switch (snapshot.connectionState) {
+                case ConnectionState.none:
+                case ConnectionState.waiting:
+                  return const Center(
+                    child: Text(
+                      'Carregando Dados...',
+                      style: TextStyle(color: Colors.green, fontSize: 25.0),
+                      textAlign: TextAlign.center,
+                    ),
+                  );
+                default:
+                  if (snapshot.hasError) {
+                    return const Center(
+                      child: Text(
+                        'Erro ao carregar dados :(',
+                        style: TextStyle(color: Colors.amber, fontSize: 25.0),
+                        textAlign: TextAlign.center,
+                      ),
+                    );
+                  } else {
+                    title = snapshot.data!["title"];
+                    img = snapshot.data!["thumbnailUrl"];
+                    return Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text(title, style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold), textAlign: TextAlign.center,),
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(20),
+                            child: Expanded(
+                              child: Image.network(img))),
+                        ],
+                      ),
+                    );
+                  }
+              }
+            }));
+  }
+}
--- treinando_flutter/lib/main.dart
+++ treinando_flutter/lib/main.dart
@@ -1,50 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-// ignore: unused_import
-import 'dart:convert' as convert;
-import 'package:http/http.dart';
+import 'package:treinando_flutter/jsonPlaceholder.dart';
 
 
 //coordenadas pirassununga = -22.0012278478714, -47.42619858643563
-String urlWeather = 'https://api.weatherbit.io/v2.0/current?lat=-22.0012278478714&lon=-47.42619858643563&key=a8ce3c44dfa74679a38ae4e2ab51ea6e&include=minutely';
 
-void main() async {
+void main()  {
 
-  http.Response response = await get(Uri.parse(urlWeather));
-  convert.jsonDecode(response.body);
-  print(response.body);
-
-
+  // http.Response response = await get(Uri.parse(urlWeather));
+  // convert.jsonDecode(response.body);
+  // print(response.body);
   //
   runApp(const MaterialApp(
-    home: HomeApiWeather(),
+    home: HomeJsonPlaceholder(),
   ));
 }
 
-class HomeApiWeather extends StatefulWidget {
-  const HomeApiWeather({super.key});
-
-  @override
-  State<HomeApiWeather> createState() => _HomeApiWeatherState();
-}
-
-class _HomeApiWeatherState extends State<HomeApiWeather> {
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Api Weather'),
-        centerTitle: true,
-        backgroundColor: Colors.deepOrange,
-        actions: [
-          IconButton(onPressed: (){}, icon: const Icon(Icons.refresh))
-        ],
-      ),
-      body: Container(
-        child: const Text('Hello API Weather'),
-      ),
-    );
-  }
-}

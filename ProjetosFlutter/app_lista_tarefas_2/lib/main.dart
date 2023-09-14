ff887c63847bc8bb748e3ad9c904a7dc46deabd2
AugustoIseppe <augusto.iseppe@outlook.com>
2023-09-14 00:18:02 -0300

    update lista de tarefas

--- ProjetosFlutter/app_lista_tarefas_2/lib/main.dart
+++ ProjetosFlutter/app_lista_tarefas_2/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:app_lista_tarefas_2/home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
     home: App_ListaTarefas2(),
   ));
 } 

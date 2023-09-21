4033c2b514171eaf78ed2ffda1918416e175c8de
AugustoIseppe <augusto.iseppe@outlook.com>
2023-09-20 22:19:53 -0300

    add .

--- Projetos Flutter/app_testes/lib/main.dart
+++ Projetos Flutter/app_testes/lib/main.dart
@@ -1,35 +1,8 @@
+import 'package:app_testes/testeListView.dart';
 import 'package:flutter/material.dart';
-void main(){
-  runApp(MaterialApp(
-    home: Teste(),
+
+void main() {
+  runApp(const MaterialApp(
+    home: TesteListView(),
   ));
-}
-
-class Teste extends StatefulWidget {
-  const Teste({super.key});
-
-  @override
-  State<Teste> createState() => _TesteState();
-}
-
-class _TesteState extends State<Teste> {
-
-  final String name = 'Augusto';
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Hello $name'),
-        centerTitle: true,
-        backgroundColor: Colors.blue,
-        actions: [
-          IconButton(onPressed: (){}, icon: Icon(Icons.refresh))
-        ],
-      ),
-      body: Container(
-        color: Colors.white,
-        child: Text('Hello $name'),
-      ),
-    );
-  }
 }

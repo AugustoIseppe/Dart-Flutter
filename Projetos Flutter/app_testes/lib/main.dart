6b89ed52bd3d1d5e17d49dd3f4f63ce500637121
AugustoIseppe <augusto.iseppe@outlook.com>
2023-09-25 23:27:15 -0300

    add .

--- Projetos Flutter/app_testes/lib/main.dart
+++ Projetos Flutter/app_testes/lib/main.dart
@@ -1,8 +1,8 @@
-import 'package:app_testes/testeListView.dart';
+import 'package:app_testes/02_contador_pessoas.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MaterialApp(
-    home: TesteListView(),
+    home: ContadorPessoas(),
   ));
 }

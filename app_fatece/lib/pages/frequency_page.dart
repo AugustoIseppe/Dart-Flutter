a644cfb21ae4d1592141e5503d31f94cd715b9e3
AugustoIseppe <augusto.iseppe@outlook.com>
2023-11-13 14:51:45 -0300

    add frequency page

--- app_fatece/lib/pages/frequency_page.dart
+++ app_fatece/lib/pages/frequency_page.dart
@@ -1,21 +1,187 @@
 import 'package:flutter/material.dart';
-
-class FrequencyPage extends StatefulWidget {
-  const FrequencyPage({super.key});
+class TesteListGenerate extends StatefulWidget {
+  const TesteListGenerate({super.key});
 
   @override
-  State<FrequencyPage> createState() => _FrequencyPageState();
+  State<TesteListGenerate> createState() => _TesteListGenerateState();
 }
 
-class _FrequencyPageState extends State<FrequencyPage> {
+class _TesteListGenerateState extends State<TesteListGenerate> {
+  Set<int> _expandedIndices = {};
+  final List<String> itemTitles = [
+    "Interface Homem-Máquina.2023.2",
+    "Tópicos de Matemática Aplicada.2023.2",
+    "Comportamento Organizacional.2023.2",
+    "Arquitetura e Organização de Computadores.2023.2",
+    "Ordenação e Pesquisa de Dados.2023.2",
+    "Eletiva IV. (Gestão do conhecimento).2023.2",
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Frequência"),
+        title: Text("Frequência Escolar"),
         centerTitle: true,
+        backgroundColor: Colors.blue,
+        foregroundColor: Colors.white,
       ),
-      body: Center(child: Text("This is Frequency Page"),),
+      body: SingleChildScrollView(
+      child: Column(
+        children: List.generate(itemTitles.length, (index) {
+          bool isExpanded = _expandedIndices.contains(index);
+          return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Card(
+                color: Colors.blue[200],
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    ListTile(
+                      title: Text(itemTitles[index]),
+                      trailing: IconButton(
+                        onPressed: () {
+                          setState(() {
+                              if (isExpanded) {
+                                _expandedIndices.remove(index);
+                              } else {
+                                _expandedIndices.add(index);
+                              }
+                            });
+                        },
+                        icon: Icon(isExpanded ? Icons.expand_less : Icons.expand_more)),
+                      ),
+                    if (isExpanded)
+                      Card(
+                        color: Colors.blue[100],
+                        elevation: 5,
+                        child: Column(
+                          children: [
+                            Container(
+                              decoration: BoxDecoration(
+                                border: Border.all(color: Colors.white),
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              padding: const EdgeInsets.symmetric(
+                                  vertical: 4, horizontal: 15),
+                              child: const Column(
+                                children: [
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text("Aula - 05/09/2023"),
+                                      Text(
+                                        "4/4",
+                                        style: TextStyle(
+                                            color: Colors.green,
+                                            fontWeight: FontWeight.w900),
+                                      ),
+                                    ],
+                                  ),
+                                  Divider(
+                                    color: Colors.white,
+                                  ),
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text("Aula - 12/09/2023"),
+                                      Text(
+                                        "3/4",
+                                        style: TextStyle(
+                                            color: Colors.blue,
+                                            fontWeight: FontWeight.w900),
+                                      ),
+                                    ],
+                                  ),
+                                  Divider(
+                                    color: Colors.white,
+                                  ),
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text("Aula - 20/09/2023"),
+                                      Text(
+                                        "0/4",
+                                        style: TextStyle(
+                                            color: Colors.red,
+                                            fontWeight: FontWeight.w900),
+                                      ),
+                                    ],
+                                  ),
+                                  Divider(
+                                    color: Colors.white,
+                                  ),
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text("Aula - 27/09/2023"),
+                                      Text(
+                                        "3/4",
+                                        style: TextStyle(
+                                            color: Colors.blue,
+                                            fontWeight: FontWeight.w900),
+                                      ),
+                                    ],
+                                  ),
+                                  Divider(
+                                    color: Colors.white,
+                                  ),
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text("Aula - 06/10/2023"),
+                                      Text(
+                                        "4/4",
+                                        style: TextStyle(
+                                            color: Colors.green,
+                                            fontWeight: FontWeight.w900),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Container(
+                                height: 50,
+                                child: const Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Column(
+                                      children: [
+                                        Text("Presente"),
+                                        Text("52"),
+                                      ],
+                                    ),
+                                    Chip(
+                                      label: Text("75%"),
+                                    ),
+                                    Column(
+                                      children: [
+                                        Text("Ausente"),
+                                        Text("4"),
+                                      ],
+                                    )
+                                  ],
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      )
+                  ],
+                ),
+              ),
+            );
+        }),
+      ),
+    ),
     );
   }
 }

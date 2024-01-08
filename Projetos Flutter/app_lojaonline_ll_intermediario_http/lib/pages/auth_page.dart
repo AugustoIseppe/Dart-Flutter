081e20ec5cc92e543682a3cbaa593617bc99315f
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2024-01-08 13:47:10 -0300

    inserido Aula 297 - Tela de Autenticação#02

--- Projetos Flutter/app_lojaonline_ll_intermediario_http/lib/pages/auth_page.dart
+++ Projetos Flutter/app_lojaonline_ll_intermediario_http/lib/pages/auth_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class AuthPage extends StatelessWidget {
@@ -10,18 +12,43 @@ class AuthPage extends StatelessWidget {
         children: [
           Container(
             decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                colors: [
-                  Color.fromRGBO(215, 117, 255, 0.5),
-                  Color.fromRGBO(255, 188, 117, 0.9),
-                ],
-              ),
+              gradient: LinearGradient(colors: [
+                Color.fromRGBO(215, 117, 255, 0.5),
+                Color.fromRGBO(255, 188, 117, 0.9),
+              ], begin: Alignment.topCenter, end: Alignment.bottomCenter),
             ),
           ),
           Container(
             width: double.infinity,
             child: Column(
-              children: const [],
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 10, horizontal: 70),
+                      transform: Matrix4.rotationZ(-8 * pi / 180)..translate(-10.0),
+                  decoration: BoxDecoration(
+                    color: Colors.deepOrange.shade900,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: const [
+                      BoxShadow(
+                      blurRadius: 8,
+                      color: Colors.black,
+                      offset: Offset(0, 2)
+                    )
+                    ]
+                  ),
+                  child: const Text(
+                    "Minha Loja",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontFamily: "Anton",
+                    ),
+                  ),
+                )
+              ],
             ),
           )
         ],
